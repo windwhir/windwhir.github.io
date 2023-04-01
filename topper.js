64e3d0291b037c6123f9bd4164d46869acdb9faf
windwhir <128274375+windwhir@users.noreply.github.com>
2023-04-01 22:01:20 +0800

    Update topper.js

--- topper.js
+++ topper.js
@@ -10,16 +10,17 @@ if(navigator.userAgent.indexOf("MQQ") > -1){document.getElementById("debug").inn
 if(navigator.userAgent.indexOf("WeChat") > -1){document.getElementById("debug").innerHTML=("<p style='font-size:300%;color:#ff0000;text-align:right;'>微信有诸多限制建议用浏览器打开↑↑↑</p>");}
 //应对本地下载
 if(thehref.indexOf("windwhir")<0)
-{document.getElementById("debug").innerHTML=(document.getElementById("debug").innerHTML+' 建议通过网络访问:<a href="https://windwhir.neocities.org">点击此处</a>')}
+{document.getElementById("debug").innerHTML=(document.getElementById("debug").innerHTML+' 建议通过网络访问:<a href="https://windwhir.neocities.org">Neocities</a> / <a href="https://windwhir.github.io">github</a>')}
 
 //首尾，样式，图标
-document.getElementById("topper").innerHTML=('<a href="https://windwhir.neocities.org"><img src="/windwhir.ico" />风吹呼呼WindWhir的neocities空间</a>');
+document.getElementById("topper").innerHTML=('<a href="/"><img src="/windwhir.ico" />风吹呼呼WindWhir的个人空间</a>');
+document.getElementById("footer").innerHTML=(document.getElementById("footer").innerHTML+'<a href="https://space.bilibili.com/347752142"><img src="/windwhir.ico" />风吹呼呼WindWhir的Bilibili主页</a>')
 //music大文件夹
 if(thehref.indexOf("music")>0){
 footdone=1;
 document.getElementById("head").innerHTML=(document.getElementById("head").innerHTML+'<link rel="icon" href="/music/music.ico" /><title>'+document.getElementById("most").innerHTML+' - 琴&风 - 风吹呼呼</title>');
 document.getElementById("topper").innerHTML=(document.getElementById("topper").innerHTML+' &gt; <a href="/music"><img src="/music/music.ico">琴&风Music</a>');
-document.getElementById("footer").innerHTML=(document.getElementById("footer").innerHTML+'<a href="https://space.bilibili.com/347752142"><img src="/windwhir.ico" />风吹呼呼WindWhir的Bilibili主页</a> - 鄙人作词作曲编曲 - music大文件内，蓝奏云密码均为whir - 歌曲及其附属所有权利保留，未经允许不得使用 - html/script by WindWhir , hosted by neocities');
+document.getElementById("footer").innerHTML=(document.getElementById("footer").innerHTML+' - 鄙人作词作曲编曲 - music大文件内，蓝奏云密码均为whir - 歌曲及其附属所有权利保留，未经允许不得使用');
 }
 //2409大文件夹
 if(thehref.indexOf("2409")>0){
@@ -27,10 +28,11 @@ footdone=1;
 document.getElementById("head").innerHTML=(document.getElementById("head").innerHTML+'<link rel="icon" href="/2409/a2409.ico" /><title>'+document.getElementById("most").innerHTML+' - 2409 - 风吹呼呼</title>');
 document.getElementById("head").innerHTML=('<link rel="icon" href="/2409.a2409.ico" />'+document.getElementById("head").innerHTML);
 document.getElementById("topper").innerHTML=(document.getElementById("topper").innerHTML+' &gt; <a href="/2409"><img src="/2409/a2409.ico">2409篇章</a>');
-document.getElementById("footer").innerHTML=(document.getElementById("footer").innerHTML+'<a href="https://space.bilibili.com/347752142"><img src="/windwhir.ico" />风吹呼呼WindWhir的Bilibili主页</a> - 郑州一中2409+x - 2409大文件内，蓝奏云密码均为2409 - 所有权利保留，仅供学习交流使用 - html/script by WindWhir , hosted by neocities');
+document.getElementById("footer").innerHTML=(document.getElementById("footer").innerHTML+' - 郑州一中2409+x - 2409大文件内，蓝奏云密码均为2409 - 所有权利保留，仅供学习交流使用');
 }
 //否则
 if(footdone==0){
 document.getElementById("head").innerHTML=(document.getElementById("head").innerHTML+'<link rel="icon" href="/windwhir.ico" /><title>'+document.getElementById("most").innerHTML+' - 风吹呼呼</title>');
-document.getElementById("footer").innerHTML=(document.getElementById("footer").innerHTML+'<a href="https://space.bilibili.com/347752142"><img src="/windwhir.ico" />风吹呼呼WindWhir的Bilibili主页</a> - html/script by WindWhir , hosted by neocities')
 }
+
+document.getElementById("footer").innerHTML=(document.getElementById("footer").innerHTML+' - html/script by WindWhir , hosted by <a href="https://windwhir.neocities.org">neocities</a> / <a href="https://windwhir.github.io">github</a> - <b style="color:white">当访问速度慢时，建议您点击前方白色链接更换访问方式</b>')
